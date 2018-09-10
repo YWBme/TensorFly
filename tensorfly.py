de65adfd08d3389faf20db6f278b586bd8aadd86
YWBme <993901096@qq.com>
2018-09-11 00:23:10 +0800

    update 0911

--- tensorfly.py
+++ tensorfly.py
@@ -255,22 +255,120 @@ print(tf.__version__)
 imdb = keras.datasets.imdb
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
 
+# Explorer the data
+print("Training entries: {}, labels: {}".format(len(train_data), len(train_labels)))
 
-添加一行，实验git
+print(train_data[0])
+
+len(train_data[0]), len(train_data[1])
+
+# import pandas as pd
+# A dictionary mapping words to an integer index
+word_index = imdb.get_word_index()
+# The first indices are reserved
+word_index = {k:(v+3) for k,v in word_index.items()}
+word_index["<PAD>"] = 0
+word_index["<START>"] = 1
+word_index["<UNK>"] = 2  # unknown
+word_index["<UNUSED>"] = 3
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+def decode_review(text):
+    return ' '.join([reverse_word_index.get(i, '?') for i in text])
+
+decode_review(train_data[0])
+decode_review(train_data[1])
 
 
-import tensorflow as tf
-from tensorflow import keras
-import numpy as np
-print(tf.__version__)
+# Prepare the data
+train_data = keras.preprocessing.sequence.pad_sequences(train_data,
+                                                        value=word_index["<PAD>"],
+                                                        padding='post',
+                                                        maxlen=256)
+test_data = keras.preprocessing.sequence.pad_sequences(test_data,
+                                                       value=word_index["<PAD>"],
+                                                       padding='post',
+                                                       maxlen=256)
+max_imdb = 0
+for i in range(len(train_data)):
+    if len(train_data[i]) > max_imdb:
+        max_imdb = len(train_data[i])
+print(max_imdb)
 
-# Download the IMDB dataset
-imdb = keras.datasets.imdb
-(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+len(train_data[0]), len(train_data[1])
+print(train_data[0])
+print(train_data[1])
+
+
+# Build the model
+
+vocab_size = 10000
+model = keras.Sequential()
+model.add(keras.layers.Embedding(vocab_size, 16))
+model.add(keras.layers.GlobalAveragePooling1D())
+model.add(keras.layers.Dense(16, activation=tf.nn.relu))
+model.add(keras.layers.Dense(1,activation=tf.nn.sigmoid))
+model.summary()
+
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+
+# Create a validation set
+x_val = train_data[:10000]
+partial_x_train = train_data[10000:]
+y_val = train_labels[:10000]
+partial_y_train = train_labels[10000:]
 
 
 
+# Train the model
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=40,
+                    batch_size=512,
+                    validation_data=(x_val, y_val),
+                    verbose=1)
 
+
+# Evaluate the model
+results = model.evaluate(test_data, test_labels)
+print(results)
+
+# Create a graph of accuracy and loss over time
+
+history_dict = history.history
+history_dict.keys()
+
+
+import matplotlib.pyplot as plt
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+# "bo" is for "blue dot"
+plt.plot(epochs, loss, 'bo', label='Training loss')
+# b is for "solid blue line"
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+
+plt.clf()   # clear figure
+acc_values = history_dict['acc']
+val_acc_values = history_dict['val_acc']
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
 
 
 
