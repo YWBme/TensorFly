c8371d1e05fce9bf0421afebbc84c771695fcef2
YWBme <993901096@qq.com>
2018-09-12 00:24:40 +0800

    0912

--- tensorfly.py
+++ tensorfly.py
@@ -374,6 +374,285 @@ plt.show()
 
 
 
+# ----------------------------------------------------------------------------------------------------------------------
+# Predict house prices: regression  |  TensorFlow
+# ----------------------------------------------------------------------------------------------------------------------
+# https://www.tensorflow.org/tutorials/keras/basic_regression
+
+from __future__ import absolute_import, division, print_function
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+print(tf.__version__)
+
+
+# The Boston Housing Prices dataset
+
+boston_housing = keras.datasets.boston_housing
+
+(train_data, train_labels), (test_data, test_labels) = boston_housing.load_data()
+
+# Shuffle the training set
+order = np.argsort(np.random.random(train_labels.shape))
+train_data = train_data[order]
+train_labels = train_labels[order]
+
+# Examples and features
+print("Training set: {}".format(train_data.shape))  # 404 examples, 13 features
+print("Testing set:  {}".format(test_data.shape))   # 102 examples, 13 features
+
+print(train_data[0])  # Display sample features, notice the different scales
+
+import pandas as pd
+column_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD',
+                'TAX', 'PTRATIO', 'B', 'LSTAT']
+df = pd.DataFrame(train_data, columns=column_names)
+df.head()
+
+print(train_labels[0:10])  # Display first 10 entries
+
+
+# Normalize features
+
+# Test data is *not* used when calculating the mean and std
+mean = train_data.mean(axis=0)
+std = train_data.std(axis=0)
+train_data = (train_data - mean) / std
+test_data = (test_data - mean) / std
+print(train_data[0])  # First training sample, normalized
+
+
+# Create the model
+def build_model():
+    model = keras.Sequential([
+        keras.layers.Dense(64, activation=tf.nn.relu,
+                           input_shape=(train_data.shape[1],)),
+        keras.layers.Dense(64, activation=tf.nn.relu),
+        keras.layers.Dense(1)
+    ])
+    optimizer = tf.train.RMSPropOptimizer(0.001)
+    model.compile(loss='mse',
+                  optimizer=optimizer,
+                  metrics=['mae'])
+    return model
+
+model = build_model()
+model.summary()
+
+
+# Train the model
+
+class PrintDot(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs):
+        if epoch % 100 == 0:
+            print('')
+        print('.', end='')
+
+EPOCHS = 500
+
+history = model.fit(train_data, train_labels, epochs=EPOCHS,
+                    validation_split= 0.2, verbose=0,
+                    callbacks=[PrintDot()])
+
+
+
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'qt5')
+
+def plot_history(history):
+    plt.figure()
+    plt.xlabel('Epochs')
+    plt.ylabel('Mean Abs Error [1000$]')
+    plt.plot(history.epoch, np.array(history.history['mean_absolute_error']),
+             label = 'Train loss')
+    plt.plot(history.epoch, np.array(history.history['val_mean_absolute_error']),
+             label = 'Test loss')
+    plt.legend()
+    plt.ylim([0,5])
+
+plot_history(history)
+
+
+
+# Use callback
+model = build_model()
+# The patience parameter is the amount of epochs to check for improvement
+early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=20)
+history = model.fit(train_data, train_labels, epochs=EPOCHS,
+                    validation_split=0.2, verbose=0,
+                    callbacks=[early_stop, PrintDot()])
+plot_history(history)
+
+
+
+[loss, mae] = model.evaluate(test_data, test_labels, verbose=0)
+print("Testing set Mean Abs Error: ${:7.2f}".format(mae * 1000))
+
+
+
+# Predict
+
+test_predictions = model.predict(test_data).flatten()
+plt.scatter(test_labels, test_predictions)
+plt.xlabel('True Values [1000$]')
+plt.ylabel('Predictions [1000$]')
+plt.axis('equal')
+plt.xlim(plt.xlim())
+plt.ylim(plt.ylim())
+_ = plt.plot([-100, 100], [-100, 100])
+
+
+
+
+error = test_predictions - test_labels
+plt.hist(error, bins = 50)
+plt.xlabel("Prediction Error [1000$]")
+_ = plt.ylabel("Count")
+
+
+# Conclusion
+# This notebook introduced a few techniques to handle a regression problem.
+#
+# Mean Squared Error (MSE) is a common loss function used for regression problems (different than classification problems).
+# Similarly, evaluation metrics used for regression differ from classification. A common regression metric is Mean Absolute Error (MAE).
+# When input data features have values with different ranges, each feature should be scaled independently.
+# If there is not much training data, prefer a small network with few hidden layers to avoid overfitting.
+# Early stopping is a useful technique to prevent overfitting.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
